e8f0cfe79c525f08c5fc2018a163b3d27841c345
Dillon McNulty <dillonmcnulty@Dillons-MacBook-Pro.local>
2026-03-22 22:05:27 -0500

    port fix

--- Python/src/main.py
+++ Python/src/main.py
@@ -30,4 +30,5 @@ def chart():
     return "No data found for that symbol."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081)
+    port = int(os.environ.get("PORT", 8081))
+    app.run(host="0.0.0.0", port=port)
